c71cac2b6d702de7ce1ccc471338d37920d8fed2
RuslanOm <ruslangfwm@gmail.com>
2020-12-15 21:49:22 +0300

    flask

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, send_file
+from skimage.util import img_as_ubyte
 import cv2
 import numpy as np
 from utils import *
 
 app = Flask(__name__)
 
-checkpoint.restore(tf.train.latest_checkpoint("checkpoints/term_project/training_checkpoints/"))
+checkpoint.restore(tf.train.latest_checkpoint("/home/ruslan/ml_project_pm/BlurFilter/checkpoints/term_project/"
+                                              "training_checkpoints/"))
 model_blurr = checkpoint.generator
 
 
 def get_image(data):
     jpg_as_np = np.frombuffer(data.read(), dtype=np.uint8)
-    img = cv2.imdecode(jpg_as_np, flags=1)
+    img = cv2.resize(cv2.imdecode(jpg_as_np, flags=1), (256, 256))
     return img
 
 
@@ -20,7 +22,7 @@ def blurr():
     src = get_image(request.files["src"])
     inp = image_to_tensor(src)
     preds = model_blurr(inp, training=False)
-    return "ok"
+    return img_as_ubyte(np.array(preds[0] * 0.5 + 0.5))
 
 
 @app.route('/style', methods=["GET", "POST"])
